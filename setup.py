27eae4c09999194d6d8a1dc66f6a10e13f681c49
VampireTechnologyLord <75166884+VampireTechnologyLord@users.noreply.github.com>
2021-06-19 21:40:09 +0200

    readying for PyPI

---
+++ setup.py
@@ -0,0 +1,28 @@
+from sys import version
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = "0.7.4"
+DESCRIPTION: "TI-Python Debugging"
+
+setup(
+    name="ti-python-module",
+    version=VERSION,
+    author="VampireTechnologyLord",
+    author_email="<elias.freund@ewe.net>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=["python", "ti", "texas instruments"],
+    classifiers=[
+        "Developement Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operation System :: Unix",
+        "Operation System :: MacOS, MacOS X",
+        "Operation System :: Microsoft :: Windows",
+    ]
+)
---
+++ ti_python/__init__.py
@@ -0,0 +1,5 @@
+from ti_rover import *
+from ti_system import *
+from ti_hub import *
+from ti_draw import *
+from __err import *
